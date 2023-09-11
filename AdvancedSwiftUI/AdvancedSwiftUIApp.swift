ea2466754295eedcbc95d2a4f8964b867a929180
VietPhuongDo <vietphuongdo511@gmail.com>
2023-09-11 15:00:14 +0700

    Initial Commit

---
+++ AdvancedSwiftUI/AdvancedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIApp.swift
+//  AdvancedSwiftUI
+//
+//  Created by PhuongDo on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
